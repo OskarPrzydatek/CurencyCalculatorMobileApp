80115f5c896a621266e6e2016e53e0c00cbf3e72
OskarPrzydatek <o.przydatek@wp.pl>
2023-06-07 13:31:05 +0200

    add calculator layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,32 +1,100 @@
+/**
+ *
+ * Layouts phasade used to manage all layouts in app development
+ *
+ * @author Oskar Przydatek
+ * @author Jakub Świderski
+ *
+ */
 import {NativeStackHeaderProps} from '@react-navigation/native-stack';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import {Appbar} from 'react-native-paper';
+import {ActivityIndicator, Appbar, Text} from 'react-native-paper';
 
-interface ICenterLayout {
+// ScreenLayout props interface
+interface IScreenLayout {
+  isLoading?: boolean;
+  error?: boolean;
   children: React.ReactNode;
 }
 
-const styles = StyleSheet.create({
-  centerLayout: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 15,
-  },
-});
+/**
+ *
+ * Layout used in all screens in app.
+ *
+ * @param {boolean?} isLoading - render loading layout when app fetch data
+ * from API
+ *
+ * @param {boolean?} error - render error layout if something went wrong
+ * with API data fetch
+ *
+ * @param {React.ReactNode} children - props to render screen layout when
+ * all data will be fetched correctly by wrapping custom screen layout.
+ *
+ * @returns - React function component
+ *
+ */
+export const ScreenLayout: React.FC<IScreenLayout> = ({
+  isLoading,
+  error,
+  children,
+}: IScreenLayout) => {
+  // Loading layout
+  if (isLoading)
+    return (
+      <View style={styles.loadingLayout}>
+        <Text>Loading Currencies</Text>
+        <ActivityIndicator />
+      </View>
+    );
 
-export const ScreenLayout: React.FC<ICenterLayout> = ({children}) => {
-  return <View style={styles.centerLayout}>{children}</View>;
+  // Fetched data error layout
+  if (error)
+    return (
+      <View style={styles.errorLayout}>
+        <Text>Something went wrong. Check internet connection</Text>
+      </View>
+    );
+
+  // Fetched data correctly layout
+  return <View style={styles.screenLayout}>{children}</View>;
 };
 
-export const CustomNavigationBar = ({
+export const CustomNavigationBar: React.FC<NativeStackHeaderProps> = ({
   navigation,
   back,
 }: NativeStackHeaderProps) => {
   return (
-    <Appbar.Header>
+    <Appbar.Header style={styles.navigationBar}>
       {back ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
     </Appbar.Header>
   );
 };
+
+/**
+ *
+ * CSS-in-JS styles object created by using ReactNative API
+ * to style all layouts
+ *
+ */
+const styles = StyleSheet.create({
+  loadingLayout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorLayout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  screenLayout: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 15,
+  },
+  navigationBar: {
+    backgroundColor: 'transparent',
+  },
+});
--- src/screens/currencies/CurrencyCalculatorScreen.tsx
+++ src/screens/currencies/CurrencyCalculatorScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Button, Text} from 'react-native-paper';
+import {Button, Divider, Text, TextInput} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
 import {ScreenLayout} from '../../layouts';
 import useSWR from 'swr';
 import {getDataFetcher} from '../../api';
+import {View} from 'react-native';
 
 export const CurrencyCalculatorScreen: React.FC = () => {
   const navigation = useNavigation<any>();
@@ -15,12 +16,44 @@ export const CurrencyCalculatorScreen: React.FC = () => {
   const onPressNavigateToCurrenciesList = () =>
     navigation.navigate('CurrenciesList');
 
+  if (data) {
+    console.log(data[0].rates);
+  }
+
   return (
-    <ScreenLayout>
-      <Text variant="headlineLarge">Currency Calculator Screen</Text>
-      <Button mode="contained" onPress={onPressNavigateToCurrenciesList}>
-        Currencies List
-      </Button>
+    <ScreenLayout isLoading={isLoading} error={error}>
+      {/* Check data will fetched, If not return null for Virtual DOM */}
+      {data ? (
+        <>
+          <Text variant="headlineLarge">Kalkulator Walut</Text>
+          <View style={{display: 'flex', flexDirection: 'row'}}>
+            <Button mode="contained" onPress={() => {}}>
+              Wybierz walutę nr. 1
+            </Button>
+            <Button mode="contained" onPress={() => {}}>
+              Wybierz walutę nr. 2
+            </Button>
+          </View>
+          <TextInput
+            mode="outlined"
+            label="Wartość waluty nr. 1"
+            style={{width: '70%'}}
+          />
+          <TextInput
+            mode="outlined"
+            label="Wartość waluty nr. 2"
+            style={{width: '70%'}}
+          />
+          <Divider style={{width: '90%'}} />
+          <Text variant="headlineMedium">X Waluta 1 = Y Waluta 2</Text>
+          <Button
+            style={{marginTop: '35%'}}
+            mode="contained"
+            onPress={onPressNavigateToCurrenciesList}>
+            Pokaż listę dostępnych walut
+          </Button>
+        </>
+      ) : null}
     </ScreenLayout>
   );
 };
